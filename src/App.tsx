0ebc0396b2863ed85ee6110e555ef70527c21e25
Anastasiia Chikrizova <78729920+Anastasiia-Chikrizova@users.noreply.github.com>
2026-08-11 23:45:21 +0300

    Set up initial React + TS + Vite (#1) (#4)
    * Set up initial React + TS + Vite
    
    * Add path alias, module scss setup, and global styles
    
    ---------
    
    Co-authored-by: Borys Torzhanskyi <u5135039754@gmail.com>
    Co-authored-by: Your Name <u513509754@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import './styles/global.module.scss';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
